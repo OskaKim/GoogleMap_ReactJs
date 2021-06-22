0206c8bb76c2fd5070b2d795205d91ca9402aaf9
Dohyeong Kim <o2kim0225@naver.com>
2021-06-22 23:00:29 +0900

    display simple google map

--- map/src/App.js
+++ map/src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import GoogleMapReact from 'google-map-react';
+
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ height: '100vh', width: '100%' }}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "AIzaSyC7uZ8u21767NHGvR5joz8bUC5FIluK18w" }}
+        defaultCenter={{lat:35.5362755,lng:139.6355183}}
+        defaultZoom={17}
+      >
+        <AnyReactComponent
+          lat={35.5362755}
+          lng={139.6355183}
+          text="My Marker"
+        />
+      </GoogleMapReact>
     </div>
   );
 }
