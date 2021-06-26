e96175a16a5c42a6c1462cb093d1f89000637f66
Dohyeong Kim <o2kim0225@naver.com>
2021-06-26 21:05:36 +0900

    seperate map file

--- map/src/App.js
+++ map/src/App.js
@@ -1,25 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
-import GoogleMapReact from 'google-map-react';
-
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
+import Map from './Map';
 
 function App() {
-  return (
-    <div style={{ height: '100vh', width: '100%' }}>
-      <GoogleMapReact
-        bootstrapURLKeys={{ key: "AIzaSyC7uZ8u21767NHGvR5joz8bUC5FIluK18w" }}
-        defaultCenter={{lat:35.5362755,lng:139.6355183}}
-        defaultZoom={17}
-      >
-        <AnyReactComponent
-          lat={35.5362755}
-          lng={139.6355183}
-          text="My Marker"
-        />
-      </GoogleMapReact>
-    </div>
-  );
+  return <Map />;
 }
 
 export default App;
---
+++ map/src/Map.js
@@ -0,0 +1,23 @@
+import GoogleMapReact from 'google-map-react';
+
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
+function Map() {
+  return (
+    <div style={{ height: '100vh', width: '100%' }}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "AIzaSyC7uZ8u21767NHGvR5joz8bUC5FIluK18w" }}
+        defaultCenter={{lat:35.5362755,lng:139.6355183}}
+        defaultZoom={17}
+      >
+        <AnyReactComponent
+          lat={35.5362755}
+          lng={139.6355183}
+          text="My Marker"
+        />
+      </GoogleMapReact>
+    </div>
+  );
+}
+
+export default Map;
