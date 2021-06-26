5c241eeadea7d7b9c36c2350049d5788fd81d8e9
Dohyeong Kim <o2kim0225@naver.com>
2021-06-26 22:39:57 +0900

    add env

--- map/src/Map.js
+++ map/src/Map.js
@@ -1,23 +1,26 @@
 import GoogleMapReact from 'google-map-react';
+import React from 'react';
 
+const API_KEY = process.env.REACT_APP_GOOGLE_MAP_API_KEY
 const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
-function Map() {
-  return (
-    <div style={{ height: '100vh', width: '100%' }}>
-      <GoogleMapReact
-        bootstrapURLKeys={{ key: "AIzaSyC7uZ8u21767NHGvR5joz8bUC5FIluK18w" }}
-        defaultCenter={{lat:35.5362755,lng:139.6355183}}
-        defaultZoom={17}
-      >
-        <AnyReactComponent
-          lat={35.5362755}
-          lng={139.6355183}
-          text="My Marker"
-        />
-      </GoogleMapReact>
-    </div>
-  );
+class Map extends React.Component {
+  render() {
+    return (
+      <div style={{ height: '100vh', width: '100%' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: API_KEY }}
+          defaultCenter={{ lat: 35.5362755, lng: 139.6355183 }}
+          defaultZoom={17}
+        >
+          <AnyReactComponent
+            lat={35.5362755}
+            lng={139.6355183}
+            text="My Marker"
+          />
+        </GoogleMapReact>
+      </div>
+    );
+  }
 }
-
 export default Map;
